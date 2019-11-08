d6281c856f247e39154f0bbb6eb6f4d6d0920c6a
zhaoyunfei <zhaoyunfei@juefx.com>
2019-11-08 13:41:04 +0800

    add flag

--- download_163music.go
+++ download_163music.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net/http"
-	"time"
 	"io"
+	"net/http"
 	"os"
+	"time"
+
 	"github.com/gocolly/colly"
 	"github.com/tidwall/gjson"
-
 	// "net/http"
 	// "github.com/PuerkitoBio/goquery"
 	// "github.com/levigross/grequests"
@@ -18,43 +19,68 @@ import (
 	// "strconv"
 )
 
-func main() {
-	// https://music.163.com/song?id=1357785909
+// 实际中应该用更好的变量名
+var (
+	dir string
+	url string
+)
 
-	// download_url := "http://music.163.com/song/media/outer/url?id=436514312.mp3"
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func main() {
 	downloadURL := "http://music.163.com/song/media/outer/url?id="
-	//mainURL := "https://music.163.com/album?id=36457807"
-	//mainURL := "https://music.163.com/artist?id=5771"
-	mainURL := "https://music.163.com/playlist?id=368529707"
-	// mainURL := "https://music.163.com/playlist?id=2319166657"
-	c := colly.NewCollector(
-	// Visit only root url and urls which start with "e" or "h" on httpbin.org
-	// colly.URLFilters(
-	// 	regexp.MustCompile("http://ps.youxiake\\.com/.*"),
-	// ),
-	// colly.AllowedDomains("music.163.com"),
-	)
-// #song-list-pre-cache
+
+	flag.StringVar(&dir, "dir", ".", "the dir download")
+	flag.StringVar(&url, "url", "", "the url of 163 music")
+	flag.Parse()
+	if 7 >= len(url) {
+		flag.Usage()
+		return
+	}
+
+	// 另一种绑定方式
+	// q = flag.Bool("q", false, "suppress non-error messages during configuration testing")
+
+	mainURL := url
+	// mainURL := "https://music.163.com/playlist?id=1988668862"
+	if exsit, err := PathExists(dir); !exsit {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			fmt.Println("dir can't create, %+v", err)
+			return
+		}
+	}
+	c := colly.NewCollector()
+	// #song-list-pre-cache
 	c.OnHTML("#song-list-pre-cache .f-hide", func(e *colly.HTMLElement) {
-		e.ForEach("a[href]", func(index int, item *colly.HTMLElement){
+		e.ForEach("a[href]", func(index int, item *colly.HTMLElement) {
 			name := item.Text
 			href := item.Attr("href")
 			id := href[9:]
 			fmt.Println("name:", name, "    id:", id)
-			durl := downloadURL+id
-			filename := name + ".mp3"
+			durl := downloadURL + id
+			filename := dir + "/" + name + ".mp3"
 			fmt.Println("begin download mp3:", name)
 			file, err := os.Create(filename)
 			defer file.Close()
 			client := http.DefaultClient
-			client.Timeout = time.Second*60
+			client.Timeout = time.Second * 60
 			resp, err := client.Get(durl)
 			if resp.ContentLength <= 0 {
 				fmt.Println("length is error!")
 			}
 			raw := resp.Body
 			defer raw.Close()
-			_,err = io.Copy(file, raw)
+			_, err = io.Copy(file, raw)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -96,20 +122,11 @@ func main() {
 			}
 
 		}
-
-		// Only those links are visited which are matched by  any of the URLFilter regexps
-		// c.Visit(e.Request.AbsoluteURL(link+"/"))
 	})
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
-		// r.Headers.Add("Referrer Policy", "no-referrer-when-downgrade")
-		// r.Headers.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
-	})
-
-	c.OnResponse(func(r *colly.Response) {
-		// fmt.Println("response body: ", string(r.Body[:]))
 	})
 
 	// Start scraping on http://httpbin.org
